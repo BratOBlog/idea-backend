4cfd5bf42328b024b0d132e5e08a94aa58b3d93d
Tibor Koós <105804739+BratOBlog@users.noreply.github.com>
2025-09-01 19:17:32 +0200

    remove console.log from db.js

--- config/db.js
+++ config/db.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 const connectDb = async () => {
     try {
         const connect = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Mongo DB Connection: ${connect.connection.host}`)
-
     }
     catch (err) {
         console.err(`Error: ${err.messsage}`)
