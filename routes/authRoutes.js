59f402ae446f90144c817b2b4c67df7804796737
Tibor Koós <bratoblog@gmail.com>
2025-09-04 09:37:36 +0200

    Update auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -41,7 +41,7 @@ router.post('/register', async (req, res, next) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: 'none',
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         })
 
@@ -99,7 +99,7 @@ router.post("/login", async (req, res, next) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: 'none',
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
             maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         })
 
@@ -124,8 +124,7 @@ router.post("/logout", (req, res) => {
     res.clearCookie("refreshToken", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: 'none'
-
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
     })
 
     res.status(200).json({ message: "Logged out successfully" })
